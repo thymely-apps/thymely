72d6b3c070c9ba8e4ec1a52d224599cbfeca4fd5
Joe Ivans <joseph.ivans@gmail.com>
2021-08-02 15:07:14 -0700

    Add application driver script

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 'use strict';
 
+/* Application Driver Class */
+
+// The activity repository contains a ton of activities
+// We're going to filter them based on "criteria", a.k.a. predicate
+//  --> applyFilter(activityLevel, thrillLevel)
+// The activity repository might have the the filter as a prototype method.
+const activityRepository = new ActivityRepository([]);
